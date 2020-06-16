0db953ad21a3bc02b0463d1c717cc2adf4324c88
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2020-06-16 15:39:17 -0500

    Add buy token functionality and tests

--- contracts/TauroTokenSale.sol
+++ contracts/TauroTokenSale.sol
@@ -5,10 +5,32 @@ contract TauroTokenSale {
   address admin;
   TauroToken public tokenContract;
   uint256 public tokenPrice;
+  uint256 public tokensSold;
+
+  event Sell(address _buyer, uint256 _amount);
 
   constructor(TauroToken _tokenContract, uint256 _tokenPrice) public {
-      admin = msg.sender;
-      tokenContract = _tokenContract;
-      tokenPrice = _tokenPrice;
+    admin = msg.sender;
+    tokenContract = _tokenContract;
+    tokenPrice = _tokenPrice;
+  }
+
+  function multiply(uint x, uint y) internal pure returns(uint z) {
+    require(y == 0 || (z = x * y) / y == x, 'Failed multiplication');
+  }
+
+  function buyTokens(uint _numberOfTokens) public payable {
+    // Require that value is equal to tokens
+    require(msg.value == multiply(_numberOfTokens, tokenPrice), 'Not enough value to buy tokens');
+
+    // Require the contract has enough tokens
+    require(tokenContract.balanceOf(address(this)) >= _numberOfTokens, 'Not enough tokens in contract');
+
+    // Require that a transfer is succesful
+    require(tokenContract.transfer(msg.sender, _numberOfTokens), 'Require that a transfer is succesful');
+
+    tokensSold += _numberOfTokens;
+
+    emit Sell(msg.sender, _numberOfTokens);
   }
 }
--- test/TauroTokenSale.js
+++ test/TauroTokenSale.js
@@ -1,7 +1,12 @@
-var TauroTokenSale = artifacts.require('TauroTokenSale')
+const TauroTokenSale = artifacts.require('TauroTokenSale')
+const TauroToken = artifacts.require('TauroToken')
 
 contract('TauroTokenSale', (accounts) => {
-  var tokenPrice = 1000000000000000 // in wei
+  const tokenPrice = 1000000000000000 // in wei
+  const buyer = accounts[1]
+  let numberOfTokens
+  const admin = accounts[0]
+  const tokensAvailable = 750000
 
   it('initializes contract with the corect values', async  () => {
     const tokenSaleInstance = await TauroTokenSale.deployed()
@@ -13,5 +18,42 @@ contract('TauroTokenSale', (accounts) => {
 
     const price = await tokenSaleInstance.tokenPrice()
     assert.equal(price, tokenPrice, 'token price is correct')
+  })
+
+  it('facilitates token buying', async () => {
+    const tokenSaleInstance = await TauroTokenSale.deployed()
+    const tokenInstance = await TauroToken.deployed()
+
+    // Provision 75% of all tokens to the token sale
+    tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, { from: admin })
+
+    numberOfTokens = 10
+
+    const receipt = await tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: numberOfTokens * tokenPrice})
+    assert.equal(receipt.logs.length, 1, 'triggers one event')
+    assert.equal(receipt.logs[0].event, 'Sell', 'should be Sell event')
+    assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchased the tokens')
+    assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of tokens purchased')
+
+    const amount = await tokenSaleInstance.tokensSold()
+    assert.equal(amount.toNumber(), numberOfTokens, 'increments the number of tokens sold')
+
+    const balanceContract = await tokenInstance.balanceOf(tokenSaleInstance.address)
+    assert.equal(balanceContract.toNumber(), tokensAvailable - numberOfTokens)
+
+    const balanceBuyer = await tokenInstance.balanceOf(buyer)
+    assert.equal(balanceBuyer.toNumber(), numberOfTokens)
+
+    try {
+      await tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: 1})
+    } catch(err) {
+      assert(err.message.includes('revert'), true, 'msg.value must equal number fo tokens in wei')
+    }
+    // Buy more tokens than available in the contract sale
+    try {
+      await tokenSaleInstance.buyTokens(800000, { from: buyer, value: 1})
+    } catch(err) {
+      assert(err.message.includes('revert'), true, 'cannot purchase more tokens than availble in contract sale')
+    }
   })
 })
